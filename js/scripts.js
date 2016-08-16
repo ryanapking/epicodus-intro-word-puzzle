428b1652c89b6b780131077bb4e4fddd78398f97
Josh Huffman and Ryan Apking <ryan@>
2016-08-16 14:22:45 -0700

    add images that change as wrong answers are entered

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+// back end logic
+
 function lettersToHide(tempString) {
   var lettersArray = tempString.split("");
   return lettersArray;
@@ -13,9 +15,22 @@ function maskWord(word, letterArray) {
 
 function removeArrayElement(letterArray, letter) {
   var i = letterArray.indexOf(letter);
-  letterArray.splice(i, 1);
-  return letterArray;
+  if (i != -1) {
+    letterArray.splice(i, 1);
+    return true; //removed an element
+  } else {
+    return false; //did not match and remove an element
+  }
 };
+
+function wrongLetter(wrongLetterArray, letter) {
+  if (!wrongLetterArray.includes(letter)) {
+    wrongLetterArray.push(letter);
+    $("ul").append(letter);
+    $("#hangmanImg").remove();
+    $("#gameplay").append("<img id='hangmanImg' src='img/" + wrongLetterArray.length + ".jpg'</img>")
+  }
+}
 
 function addArrayElement(letterArray, element) {
   // function not currently used
@@ -24,15 +39,23 @@ function addArrayElement(letterArray, element) {
 }
 
 function displayString(wordToShow) {
-  $("h3").text("");
-  $("h3").append(wordToShow);
+  $("#wordDisplay").text("");
+  $("#wordDisplay").append(wordToShow);
 };
 
+function endGame() {
+  $("#gameover").show();
+  $("#gameplay").hide();
+}
+
+// front end logic
 
 $(document).ready(function() {
   var word, letterArray;
+  var wrongLetterArray = [];
+  var wrongGuesses = 0;
   $("#userInput").submit(function(event) {
-    word = $("#wordInput").val();
+    word = $("#wordInput").val().toUpperCase();
     letterArray = lettersToHide(word);
     var hiddenWord = maskWord(word, letterArray);
     displayString(hiddenWord);
@@ -40,10 +63,20 @@ $(document).ready(function() {
   });
 
   $("#letterForm").submit(function(event) {
-    var userInput = $("#letterInput").val();
-    removeArrayElement(letterArray, userInput);
-    var hiddenWord = maskWord(word, letterArray);
-    displayString(hiddenWord);
+    var userInput = $("#letterInput").val().toUpperCase();
+    if (userInput.length === 1) {
+      if (!removeArrayElement(letterArray, userInput)) {
+        wrongLetter(wrongLetterArray, userInput);
+      }
+      if (wrongLetterArray.length >=6) {
+        endGame();
+      }
+      var hiddenWord = maskWord(word, letterArray);
+      displayString(hiddenWord);
+    } else {
+      alert("Please enter a single letter");
+    }
+    $("#letterInput").val("");
     event.preventDefault();
   });
 });
